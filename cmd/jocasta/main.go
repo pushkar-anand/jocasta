34e63c868dc1d76b8ac6b859810f093ea8ae7aa6
Pushkar Anand <git@mail.pushkar.dev>
2026-08-30 00:06:06 +0530

    feat(setup): integrate sqlc and setup dependencies

--- cmd/jocasta/main.go
+++ cmd/jocasta/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pushkar-anand/build-with-go/logger"
 )
 
 func main() {
+	ctx := context.Background()
+
+	// Create a context that will be canceled when the OS sends a signal to the process.
+	// This will be used to gracefully shut down the application, shutting down the server and other workers.
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGINT, syscall.SIGABRT, syscall.SIGTERM)
+	defer cancel()
+
+	log := logger.New(
+		logger.WithLevel(slog.LevelDebug),
+		logger.WithFormat(logger.FormatJSON),
+	)
+
+	log.Info("test")
+
 	fmt.Println("Hello World!")
 }
