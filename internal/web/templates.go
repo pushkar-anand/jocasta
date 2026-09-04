79609a1ed06b62c872d6233e2651408efeffd8e2
Pushkar Anand <git@mail.pushkar.dev>
2026-09-04 18:45:51 +0530

    feat(web): add new template constants for devices, network, events, and scans pages

--- internal/web/templates.go
+++ internal/web/templates.go
@@ -2,8 +2,17 @@ package web
 
 const (
 	templatePageDashboard = "page/dashboard"
+	templatePageDevices   = "page/devices"
+	templatePageDevice    = "page/device"
+	templatePageNetwork   = "page/network"
+	templatePageEvents    = "page/events"
+	templatePageScans     = "page/scans"
 
-	templatePartialLiveOverview = "partial/live-body"
+	templatePartialLiveOverview  = "partial/live-body"
+	templatePartialDeviceRows    = "partial/device-rows"
+	templatePartialDeviceRow     = "partial/device-row"
+	templatePartialDeviceRowForm = "partial/device-row-form"
+	templatePartialDevicePanel   = "partial/device-panel"
 
 	TemplateNotFound = "page/notfound"
 )
